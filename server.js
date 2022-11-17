d026ba9354bb91426ca3a90dd999d8a91b02d598
shubham-am-i <patilshubham2k21@gmail.com>
2022-11-17 21:23:36 +0530

    connect with db

--- server.js
+++ server.js
@@ -2,11 +2,14 @@
 
 // external imports
 import express from 'express'
+import dotenv from 'dotenv'
 import colors from 'colors'
 
 // local imports
 import { notFound, errorHandler } from './middlewares/errorMiddleware.js'
+import connectDB from './config/db.js'
 
+dotenv.config()
 const app = express()
 
 // app.get('/', (req, res) => {
@@ -17,6 +20,12 @@ app.use(notFound)
 app.use(errorHandler)
 
 const port = process.env.port || 5000
-app.listen(port, () =>
-  console.log(`Server is running on port ${port}`.yellow.bold)
-)
+
+const bootstrap = async () => {
+  await connectDB()
+  app.listen(port, () =>
+    console.log(`Server is running on port ${port}`.yellow.bold)
+  )
+}
+
+bootstrap()
