b351e37d7372c944085dc68dbfdd51535cfdf94e
shubham-am-i <patilshubham2k21@gmail.com>
2022-11-17 11:35:38 +0530

    add Alert component

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,5 +1,5 @@
 import { useState, useEffect, useSyncExternalStore } from 'react'
-import { Logo, FormRow } from '../components'
+import { Logo, FormRow, Alert } from '../components'
 import Wrapper from '../assets/wrappers/RegisterPage'
 
 const initialState = {
@@ -7,6 +7,7 @@ const initialState = {
   email: '',
   password: '',
   isMember: true,
+  showAlert: false,
 }
 
 const Register = () => {
@@ -27,6 +28,7 @@ const Register = () => {
       <form className='form' onSubmit={onSubmit}>
         <Logo />
         <h3>Login</h3>
+        {values.showAlert && <Alert />}
         {/* name input */}
         <FormRow
           type='text'
@@ -49,7 +51,9 @@ const Register = () => {
           handleChange={handleChange}
         />
 
-        <button type='submit' className='btn btn-block'></button>
+        <button type='submit' className='btn btn-block'>
+          Submit
+        </button>
       </form>
     </Wrapper>
   )
