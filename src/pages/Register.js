e4c2a0dda7f11ade0037e4594701fc0e5849fb15
shubham-am-i <patilshubham2k21@gmail.com>
2022-11-17 12:25:42 +0530

    toggle member

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -14,6 +14,10 @@ const Register = () => {
   const [values, setValues] = useState(initialState)
   // global state and useNavigate
 
+  const toggleMember = () => {
+    setValues({ ...values, isMember: !values.isMember })
+  }
+
   const handleChange = (e) => {
     console.log(e.target)
   }
@@ -27,15 +31,17 @@ const Register = () => {
     <Wrapper className='full-page'>
       <form className='form' onSubmit={onSubmit}>
         <Logo />
-        <h3>Login</h3>
+        <h3>{values.isMember ? 'Login' : 'Register'}</h3>
         {values.showAlert && <Alert />}
         {/* name input */}
-        <FormRow
-          type='text'
-          name='name'
-          value={values.name}
-          handleChange={handleChange}
-        />
+        {!values.isMember && (
+          <FormRow
+            type='text'
+            name='name'
+            value={values.name}
+            handleChange={handleChange}
+          />
+        )}
         {/* email input */}
         <FormRow
           type='email'
@@ -54,6 +60,12 @@ const Register = () => {
         <button type='submit' className='btn btn-block'>
           Submit
         </button>
+        <p>
+          {values.isMember ? 'Not a member yet?' : 'Already a member?'}
+          <button type='button' onClick={toggleMember} className='member-btn'>
+            {values.isMember ? 'Register' : 'Login'}
+          </button>
+        </p>
       </form>
     </Wrapper>
   )
