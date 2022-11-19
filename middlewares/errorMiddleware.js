5c0b95e5a35fc69d1ded9c694df25b64f2a9b454
shubham-am-i <patilshubham2k21@gmail.com>
2022-11-19 17:42:53 +0530

    cd to jobify dir for commits

--- middlewares/errorMiddleware.js
+++ middlewares/errorMiddleware.js
@@ -34,7 +34,7 @@ const errorHandler = (err, req, res, next) => {
 
   res.status(error.statusCode || 500).json({
     success: false,
-    error: error.message || 'Server Error',
+    msg: error.message || 'Server Error',
   })
 }
 
