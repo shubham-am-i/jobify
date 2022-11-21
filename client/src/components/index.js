0d336559438f0b0eacdf955bb4c09bfbab1d55de
shubham-am-i <patilshubham2k21@gmail.com>
2022-11-21 11:59:43 +0530

    Shared Layout

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,4 +1,7 @@
 import Logo from './Logo'
 import FormRow from './FormRow'
 import Alert from './Alert'
-export { Logo, FormRow, Alert }
+import Navbar from './Navbar'
+import SmallSidebar from './SmallSidebar'
+import BigSidebar from './BigSidebar'
+export { Logo, FormRow, Alert, SmallSidebar, BigSidebar, Navbar }
