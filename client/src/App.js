b8199055406bbf55401e467d4e0c59b4ed4de264
shubham-am-i <patilshubham2k21@gmail.com>
2022-11-21 09:36:56 +0530

    Implement Nested pages And Protected Routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
-import { Landing, Error, Register } from './pages'
-export {
+import { Landing, Error, Register, ProtectedRoute } from './pages'
+import {
   AllJobs,
   Profile,
   SharedLayout,
@@ -11,7 +11,19 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<div>Dashboard</div>} />
+        <Route
+          path='/'
+          element={
+            <ProtectedRoute>
+              <SharedLayout />
+            </ProtectedRoute>
+          }
+        >
+          <Route index element={<Stats />} />
+          <Route path='all-jobs' element={<AllJobs />} />
+          <Route path='add-job' element={<AddJob />} />
+          <Route path='profile' element={<Profile />} />
+        </Route>
         <Route path='/landing' element={<Landing />} />
         <Route path='/register' element={<Register />} />
         <Route path='*' element={<Error />} />
--- client/src/pages/dashboard/index.js
+++ client/src/pages/dashboard/index.js
@@ -3,5 +3,4 @@ import AllJobs from './AllJobs'
 import Profile from './Profile'
 import SharedLayout from './SharedLayout'
 import Stats from './Stats'
-
 export { AllJobs, Profile, SharedLayout, Stats, AddJob }
